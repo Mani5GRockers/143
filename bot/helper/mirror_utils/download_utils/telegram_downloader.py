bba1940cca9285ddb495fa1e00838f0d506ce353
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-25 14:24:30 +0530

    Update telegram_downloader.py

--- bot/helper/mirror_utils/download_utils/telegram_downloader.py
+++ bot/helper/mirror_utils/download_utils/telegram_downloader.py
@@ -104,7 +104,7 @@ class TelegramDownloadHelper(DownloadHelper):
                     gd = GoogleDriveHelper()
                     smsg, button = gd.drive_list(name, True, True)
                     if smsg:
-                        sendMarkup("File/Folder is already available in Drive.\nHere are the search results:", self.__listener.bot, self.__listener.update, button)
+                        sendMarkup("➩ File/Folder is already available in Drive.\n➩ Here are the search results:", self.__listener.bot, self.__listener.update, button)
                         return
                 sendStatusMessage(self.__listener.update, self.__listener.bot)
                 self.__onDownloadStart(name, media.file_size, media.file_id)

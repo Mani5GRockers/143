0f3835c13646c4e383b917333c8a48755501daea
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-25 14:48:10 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -117,7 +117,7 @@ def get_progress_bar_string(status):
 
 def get_readable_message():
     with download_dict_lock:
-        msg = "\n✥═══ @awsmirrorzone ═══✥\n\n"
+        msg = "\n✥══ @awsmirrorzone ══✥\n\n"
         start = 0
         if STATUS_LIMIT is not None:
             dick_no = len(download_dict)
